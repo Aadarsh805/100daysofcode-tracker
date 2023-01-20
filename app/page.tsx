fa152f173518812a3dd403844c371f3ba3a065ad
aadarsh805 <aadarshthakur80501@gmail.com>
2023-01-21 01:22:38 +0530

    deps installed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="h-screen">
-      <p className="text-4xl bg-blue-400 p-4 underline">hello</p>;
+      <p className="text-4xl bg-blue-400 p-4 underline text-center">hello</p>
     </div>
   );
 }
