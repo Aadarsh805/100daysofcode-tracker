898f16cae901319fe980870da398180ee8e89f35
Aadarsh Thakur <95094057+Aadarsh805@users.noreply.github.com>
2023-01-26 22:48:53 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
-import Page from "./dashboard/page";
 
 export default function Home() {
   return (
     <div className="h-screen">
-      <Page />
+      <p>Landing Page</p>
     </div>
   );
 }
