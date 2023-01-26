cea1aad33d654ed02d2efea419a9c797a0431a68
nidhikumari <kumari9936nidhi@gmail.com>
2023-01-26 22:12:01 +0530

    added the navbar in the layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +12,13 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <nav className="flex justify-between items-center bg-[#312E47] p-5">
+          <h1>#100DaysOfCode</h1>
+          <p>Star Us</p>
+        </nav>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,6 @@ import Page from "./dashboard/page";
 export default function Home() {
   return (
     <div className="h-screen">
-      <nav className="flex justify-between items-center bg-[#312E47] p-5">
-        <h1>#100DaysOfCode</h1>
-        <p>Star Us</p>
-      </nav>
       <Page />
     </div>
   );
