84f6e7dd1a05f4cc62ea6bea3afe5c6632b8eaf9
aadarsh805 <aadarshthakur80501@gmail.com>
2023-02-19 01:26:30 +0530

    conflicts resolved

--- config/supabase.ts
+++ config/supabase.ts
@@ -1,10 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-// const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-// const supabaseKey = process.env.NEXT_PUBLIC_ANON_KEY!;
-
-const supabaseURL = 'https://localhost:8000'
-const supabaseKey = '12345657'
+const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_ANON_KEY!;
 
 const supabase = createClient(supabaseURL, supabaseKey);
 
