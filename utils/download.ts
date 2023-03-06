147e0b74938516adf3d77fac42afa93571aceaac
aadarsh805 <aadarshthakur80501@gmail.com>
2023-03-07 05:08:53 +0530

    feat: added fucntionality to donwload

---
+++ utils/download.ts
@@ -0,0 +1,13 @@
+export function download(canvas: any) {
+  try {
+    const dataUrl = canvas.toDataURL();
+    const a = document.createElement("a");
+    document.body.insertAdjacentElement("beforeend", a);
+    a.download = "contributions.png";
+    a.href = dataUrl;
+    a.click();
+    document.body.removeChild(a);
+  } catch (err) {
+    console.error(err);
+  }
+}
